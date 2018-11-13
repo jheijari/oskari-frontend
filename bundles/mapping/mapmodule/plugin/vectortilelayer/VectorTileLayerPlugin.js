d6d52056e9e3956a8f15e967897f8c5511193733
Joonas Heijari <joonas.heijari@gmail.com>
2018-11-13 14:29:47 +0200

    Remove contrib modules from sample-app

--- bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
+++ bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
@@ -7,7 +7,7 @@ import { createDefaultStyle } from 'ol/style/Style';
 import VectorTileModelBuilder from './VectorTileModelBuilder';
 import VectorTileLayer from './VectorTileLayer';
 import styleGenerator from './styleGenerator';
-import mbStylefunction from 'ol-mapbox-style/stylefunction';
+
 import { LAYER_ID, LAYER_HOVER, LAYER_TYPE, FTR_PROPERTY_ID } from '../../domain/constants';
 
 const AbstractMapLayerPlugin = Oskari.clazz.get('Oskari.mapping.mapmodule.AbstractMapLayerPlugin');
@@ -67,10 +67,6 @@ Oskari.clazz.defineES('Oskari.mapframework.mapmodule.VectorTileLayerPlugin',
         _getLayerCurrentStyleFunction (layer) {
             const hoverOptions = layer.getHoverOptions();
             const styleDef = layer.getCurrentStyleDef();
-            const mbStyle = layer.getCurrentExternalStyleDef();
-            if (mbStyle) {
-                return mbStylefunction(mbStyle);
-            }
             const factory = this.mapModule.getStyle.bind(this.mapModule);
             return styleDef ? styleGenerator(factory, styleDef, hoverOptions, this.hoverState) : createDefaultStyle;
         }
